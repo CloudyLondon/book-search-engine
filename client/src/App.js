3de8df1dd3c41b41730c1a6ecf09aa7e89c4e027
CloudyLondon <jovialcty@gmail.com>
2022-03-30 12:20:00 -0700

    modify app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,64 @@
+// modify app.js to include apollo provider
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// add apollo dependencies
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
-// Create an Apollo Provider to make every request work with the Apollo server.
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
 
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  //get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  //return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// create apollo client
+const client = new ApolloClient({
+  //set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+// Create an Apollo Provider to make every request work with the Apollo server.
+// modify App() -> add ApolloProvider tag/wrapper
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={SearchBooks} />
-          <Route exact path="/saved" component={SavedBooks} />
-          <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
 export default App;
+
+//go to utils!
